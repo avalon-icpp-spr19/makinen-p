92548e124803832713b3968ddf1e36c0bcd64f6b
degamus <RedCrafting.SIM@gmail.com>
2019-02-09 16:27:09 +0300

    Update typetable.cpp

--- typetable.cpp
+++ typetable.cpp
@@ -35,30 +35,33 @@ int main()
 	std::numeric_limits<int>::digits; // возвращает количество значимых бит переменной типа int
 
 
-	cout << "Тип переменной" << setw(10) << setw(10) << "|  " << "Размер," << setw(2) << "|" << setw(20) << "    Значение" << setw(16) << "|  " << setw(10) << "Количество" << endl;
-	
-	cout << setw(25) << "|   " << "байт" << setw(4) << "|" << setw(10) << "  Минимальное" << setw(6) << "|  " << setw(10) << "Максимальное  " << "| " << "значимых бит" << endl;
-	
-	cout << "--------------------------------------------------------------------------------" << endl;
-	
-	cout << "bool" << setw(18) << "|" << setw(5) << sizeof(bool) << setw(6) << "|" << setw(10) << std::numeric_limits<bool>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<bool>::max() << setw(7) << "|" << setw(7) << std::numeric_limits<bool>::digits << endl;
-	
-	cout << "unsigned short" << setw(8) << "|" << setw(5) << sizeof(unsigned short) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned short>::min() << setw(9) << "|" << setw(10) << std::numeric_limits<unsigned short>::max() << setw(7) << "|" << setw(8) << std::numeric_limits<unsigned short>::digits << endl;
-	
-	cout << "short" << setw(17) << "|" << setw(5) << sizeof(short) << setw(6) << "|" << setw(10) << std::numeric_limits<short>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<short>::max() << setw(7) << "|" << setw(8) << std::numeric_limits<short>::digits << endl;
-	
-	cout << "unsigned int" << setw(10) << "|" << setw(5) << sizeof(unsigned int) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned int>::min() << setw(9) << "|" << setw(13) << std::numeric_limits<unsigned int>::max() << setw(4) << "|" << setw(8) << std::numeric_limits<unsigned int>::digits << endl;
-	
-	cout << "int" << setw(19) << "|" << setw(5) << sizeof(int) << setw(6) << "|" << setw(13) << std::numeric_limits<int>::min() << setw(4) << "|" << setw(13) << std::numeric_limits<int>::max() << setw(4) << "|" << setw(8) << std::numeric_limits<int>::digits << endl;
-	
-	cout << "unsigned long" << setw(9) << "|" << setw(5) << sizeof(unsigned long) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned long>::min() << setw(9) << "|" << setw(13) << std::numeric_limits<unsigned long>::max() << setw(4) << "|" << setw(8) << std::numeric_limits<unsigned long>::digits << endl;
-	
-	cout << "long" << setw(18) << "|" << setw(5) << sizeof(long) << setw(6) << "|" << setw(10) << std::numeric_limits<long>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<long>::max() << setw(7) << "|" << setw(7) << std::numeric_limits<long>::digits << endl;
-	
-	cout << "bool" << setw(18) << "|" << setw(5) << sizeof(bool) << setw(6) << "|" << setw(10) << std::numeric_limits<bool>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<bool>::max() << setw(7) << "|" << setw(7) << std::numeric_limits<bool>::digits << endl;
-	
-	cout << "bool" << setw(18) << "|" << setw(5) << sizeof(bool) << setw(6) << "|" << setw(10) << std::numeric_limits<bool>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<bool>::max() << setw(7) << "|" << setw(7) << std::numeric_limits<bool>::digits << endl;
-	
-	cout << "bool" << setw(18) << "|" << setw(5) << sizeof(bool) << setw(6) << "|" << setw(10) << std::numeric_limits<bool>::min() << setw(7) << "|" << setw(10) << std::numeric_limits<bool>::max() << setw(7) << "|" << setw(7) << std::numeric_limits<bool>::digits << endl;
+	cout << "Тип переменной" << setw(10) << setw(10) << "|  " << "Размер," << setw(2) << "|" << setw(25) << "    Значение" << setw(25) << "|  " << setw(10) << "Количество" << endl;
 
+	cout << setw(25) << "|   " << "байт" << setw(4) << "|" << setw(10) << "  Минимальное" << setw(10) << "|  " << setw(10) << "Максимальное  " << "          |  " << "значимых бит" << endl;
+
+	cout << "------------------------------------------------------------------------------------------------" << endl;
+
+	cout << "bool" << setw(18) << "|" << setw(5) << sizeof(bool) << setw(6) << "|" << setw(10) << std::numeric_limits<bool>::min() << setw(11) << "|" << setw(10) << std::numeric_limits<bool>::max() << setw(17) << "|" << setw(7) << std::numeric_limits<bool>::digits << endl;
+
+	cout << "unsigned short" << setw(8) << "|" << setw(5) << sizeof(unsigned short) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned short>::min() << setw(13) << "|" << setw(10) << std::numeric_limits<unsigned short>::max() << setw(17) << "|" << setw(8) << std::numeric_limits<unsigned short>::digits << endl;
+
+	cout << "short" << setw(17) << "|" << setw(5) << sizeof(short) << setw(6) << "|" << setw(10) << std::numeric_limits<short>::min() << setw(11) << "|" << setw(10) << std::numeric_limits<short>::max() << setw(17) << "|" << setw(8) << std::numeric_limits<short>::digits << endl;
+
+	cout << "unsigned int" << setw(10) << "|" << setw(5) << sizeof(unsigned int) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned int>::min() << setw(13) << "|" << setw(13) << std::numeric_limits<unsigned int>::max() << setw(14) << "|" << setw(8) << std::numeric_limits<unsigned int>::digits << endl;
+
+	cout << "int" << setw(19) << "|" << setw(5) << sizeof(int) << setw(6) << "|" << setw(13) << std::numeric_limits<int>::min() << setw(8) << "|" << setw(13) << std::numeric_limits<int>::max() << setw(14) << "|" << setw(8) << std::numeric_limits<int>::digits << endl;
+
+	cout << "unsigned long" << setw(9) << "|" << setw(5) << sizeof(unsigned long) << setw(6) << "|" << setw(8) << std::numeric_limits<unsigned long>::min() << setw(13) << "|" << setw(13) << std::numeric_limits<unsigned long>::max() << setw(14) << "|" << setw(8) << std::numeric_limits<unsigned long>::digits << endl;
+
+	cout << "long" << setw(18) << "|" << setw(5) << sizeof(long) << setw(6) << "|" << setw(13) << std::numeric_limits<long>::min() << setw(8) << "|" << setw(13) << std::numeric_limits<long>::max() << setw(14) << "|" << setw(8) << std::numeric_limits<long>::digits << endl;
+
+	cout << "unsigned long long" << setw(4) << "|" << setw(5) << sizeof(unsigned long long) << setw(6) << "|" << setw(9) << std::numeric_limits<unsigned long long>::min() << setw(12) << "|" << setw(12) << std::numeric_limits<unsigned long long>::max() << setw(7) << "|" << setw(8) << std::numeric_limits<unsigned long long>::digits << endl;
+
+	cout << "long long" << setw(13) << "|" << setw(5) << sizeof(long long) << setw(6) << "|" << setw(10) << std::numeric_limits<long long>::min()  << "|" << setw(10) << std::numeric_limits<long long>::max() << setw(8) << "|" << setw(8) << std::numeric_limits<long long>::digits << endl;
+
+	cout << "char" << setw(18) << "|" << setw(5) << sizeof(char) << setw(6) << "|" << setw(10) << std::numeric_limits<char>::min() << setw(11) << "|" << setw(10) << std::numeric_limits<char>::max() << setw(17) << "|" << setw(7) << std::numeric_limits<char>::digits << endl;
+
+	cout << "float" << setw(17) << "|" << setw(5) << sizeof(float) << setw(6) << "|" << setw(10) << std::numeric_limits<float>::min() << setw(10) << "|" << setw(10) << std::numeric_limits<float>::max() << setw(16) << "|" << setw(8) << std::numeric_limits<float>::digits << endl;
+
+	cout << "double" << setw(16) << "|" << setw(5) << sizeof(double) << setw(6) << "|" << setw(10) << std::numeric_limits<double>::min() << setw(9) << "|" << setw(10) << std::numeric_limits<double>::max() << setw(15) << "|" << setw(8) << std::numeric_limits<double>::digits << endl;
 }
