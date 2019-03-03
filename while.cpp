d1aab899a534dbd49977eb748b7c98d9a6726fe7
degamus <RedCrafting.SIM@gmail.com>
2019-03-03 12:59:29 +0300

    Create while.cpp

---
+++ while.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+//ex.4
+
+//int main()
+//{
+//	int n, counter;
+//	counter = 1;
+//	cin >> n;
+//	while (n > pow(2,counter))
+//	{
+//		counter += 1;
+//	}
+//
+//	cout << (pow(2, counter)) << endl;
+//}
+
+
+//ex.5
+
+//int main()
+//{
+//
+//	int n;
+//	int n1 = 0;
+//	int n2 = 1;
+//	int n3 = 0;
+//	int counter = 0;
+//
+//	cin >> n;
+//
+//	if (n == 0) {
+//		cout << "0";
+//	}
+//	else
+//	{
+//		while (counter < n - 1)
+//		{
+//			n3 = n1 + n2;
+//			n1 = n2;
+//			n2 = n3;
+//			counter += 1;
+//		}
+//
+//		cout << n2;
+//	}
+//}
