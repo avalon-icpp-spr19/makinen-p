6708849d2f14214a2c374b6dcd9d0f8428beb1a6
degamus <RedCrafting.SIM@gmail.com>
2019-03-03 12:33:14 +0300

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Упражнение 1
+
+//int main() 
+//{
+//
+//	int N;
+//	int counter = 0;
+//	cin >> N;
+//
+//	for (int i = 100; i <= 999; i++)
+//	{
+//		if (((i % 10) + ((i / 10) % 10) + (i / 100)) == N)
+//		{
+//			counter += 1;
+//		}
+//	}
+//	cout << counter;
+//}
+
+//Упражнение 2
+
+//int main()
+//{
+//	string line;
+//	getline(cin, line);
+//	int len = line.size();
+//
+//	cout << "+";
+//
+//	for (int i = 0; i <= len+1; ++i)
+//	{
+//		cout << "-";
+//	}
+//
+//	cout << "+" << endl;
+//
+//	cout << "| " << line << " |" << endl;
+//
+//	cout << "+";
+//
+//	for (int i = 0; i <= len+1; ++i)
+//	{
+//		cout << "-";
+//	}
+//
+//	cout << "+" << endl;
+//}
+
+int main()
+{
+
+}
