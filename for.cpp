ddc9f4c75409f3f6aba2c16cfb79714fc6b45658
degamus <RedCrafting.SIM@gmail.com>
2019-03-24 10:33:18 +0300

    Update for.cpp

--- for.cpp
+++ for.cpp
@@ -51,7 +51,59 @@ using namespace std;
 //	cout << "+" << endl;
 //}
 
-int main()
-{
 
-}
+//ex 3
+
+//int main()
+//{
+//
+//	int n;
+//	cin >> n; 
+//
+//	for (int i = 0; i < n - 1; ++i)
+//	{
+//		for (int j = 1; j <= n; ++j)
+//		{
+//			j += i % 2;
+//				if (j % 2 == 0)
+//				{
+//					cout << "#";
+//				}
+//				else
+//				{
+//					cout << ".";
+//				}
+//				j -= i % 2;
+//		}
+//		cout << endl;
+//	}
+//
+//}
+
+//ex 4
+
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//
+//	for (int i = 1; i <= n*2; i=i+2)
+//	{
+//		for (int j = 1; j <= i; ++j)
+//		{
+//			cout << "#";
+//		}	
+//		cout << endl;
+//	}
+//	
+//
+//	for (int k = n*2-3; k >= 0; k = k - 2)
+//	{
+//		for (int l = 1; l <= k; ++l)
+//		{
+//			cout << "#";
+//		}
+//		cout << endl;
+//	}
+//}
+
