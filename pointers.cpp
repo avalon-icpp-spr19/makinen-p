5bb96e6cc2782c577004870273707824b442e0f4
degamus <RedCrafting.SIM@gmail.com>
2019-04-07 12:41:58 +0300

    Create pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,31 @@
+//Задание №1
+//
+//
+//int main()
+//{
+//	int val = 10;
+//	int * ptr = &val;
+//	cout << ptr << endl;
+//	cout << *ptr << endl;
+//
+//	unsigned int a = 5U;
+//	double b = 0.8;
+//	const float f = 0.5F;
+//	const long long ll = 123LL;
+//
+//	unsigned int * ptr1 = &a;
+//	double * ptr2 = &b;
+//	const float * ptr3 = &f;
+//	const long long * ptr4 = &ll;
+//
+//	cout << sizeof(ptr1) << endl;
+//	cout << sizeof(ptr2) << endl;
+//	cout << sizeof(ptr3) << endl;
+//	cout << sizeof(ptr4) << endl;
+//
+//	*ptr1 += 1;
+//	*ptr2 = 0.25;
+//	cout << *ptr * 2 << endl;
+//	cout << ll % 10 << endl;
+//
+//}
