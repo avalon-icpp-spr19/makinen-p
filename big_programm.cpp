c2aad0c631801c561dd66a6bb0031260277ac8ab
degamus <RedCrafting.SIM@gmail.com>
2019-05-12 10:16:57 +0300

    Update big_programm.cpp

--- big_programm.cpp
+++ big_programm.cpp
@@ -13,10 +13,12 @@ void RandomArrays(short int birthdays[], int people)
 
 int main()
 {
-	int TRIALS, z;
-	cin >> TRIALS;
-	while (z = 0)
+	int TRIALS;
+	int PEOPLE;
+
+	while (true)
 	{
+		cin >> TRIALS;
 		if (TRIALS < 1)
 		{
 			cout << "ERROR: Increase the amount of trials" << endl;
@@ -27,34 +29,41 @@ int main()
 		}
 		else
 		{
-			z = 1;
+			break;
 		}
 	}
-	short int birthdays[23];
+
+	cin >> PEOPLE;
+
+	short int* birthdays = new short int[PEOPLE];
 	int successfulAttempts = 0;
 	float Chance;
 	int flag = 0;
 
 
-	
+
 	for (int i = 0; i < TRIALS; i++) {
-		RandomArrays(birthdays, 23);
+		RandomArrays(birthdays, 87);
 		flag = 0;
-			for (int k = 0; k < 22 && flag == 0; k++)
+		for (int z = 0; z < PEOPLE-2 && flag == 0; z++)
+		{
+			for (int k = z+1; k < PEOPLE-1 && flag == 0; k++)
 			{
-				for (int l = k + 1; l < 23 && flag == 0; l++)
+				for (int l = k + 1; l < PEOPLE && flag == 0; l++)
 				{
-					if (birthdays[k] == birthdays[l])
+					if (birthdays[k] == birthdays[l] && birthdays[z] == birthdays[k] && birthdays[l] == birthdays[z])
 					{
 						successfulAttempts++;
 						flag = 1;
 					}
 				}
-				
 			}
+		}
 	}
 
 	Chance = 100.0*successfulAttempts / TRIALS;
 
 	cout << "After " << TRIALS << " trials, only " << successfulAttempts << " were succesful. Chance: " << Chance << "%." << endl;
-	}
+
+	return 0;
+}
